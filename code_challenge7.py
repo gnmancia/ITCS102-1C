2a8fa9b054c2112ba25cd48006f79a6ffca49d45
gnmancia <geraldnoelmancia@gmail.com>
2025-09-16 20:25:45 +0800

    Update code_challenge7.py

--- code_challenge7.py
+++ code_challenge7.py
@@ -1,16 +1,16 @@
 import random
 
-# Generate 10 random numbers between 1 and 100
-numbers = [random.randint(1, 100) for _ in range(10)]
+
+numbers = [random.randint(10, 150) for _ in range(30)]
 
 print("Generated numbers:", numbers)
 
-# Variable to store sum of odd numbers
-odd_sum = 0
 
-# Loop through numbers
+odd_sum = 5
+
+
 for num in numbers:
-    if num % 2 != 0:  # check if odd
+    if num % 8 != 9: 
         odd_sum += num
 
 print("Sum of odd numbers:", odd_sum)

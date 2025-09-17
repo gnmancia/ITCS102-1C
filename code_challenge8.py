991999c4415e0c8226c4568b087eca7314fee31b
gnmancia <geraldnoelmancia@gmail.com>
2025-09-17 08:46:08 +0800

    Add files via upload

---
+++ code_challenge8.py
@@ -0,0 +1,10 @@
+print("MULTIPLICATION TABLE MAKER")
+
+
+num = int(input("Enter a number: "))
+
+print(f"\nMultiplication table for {num}:")
+
+for i in range(1, 11):
+    result = num * i
+    print(f"{num} x {i} = {result}")
---
+++ code_challenge9.py
@@ -0,0 +1,9 @@
+print("COUNTDOWN TIMER SIMULATOR")
+
+start = int(input("Enter the starting number for countdown: "))
+
+print("\nCountdown started:")
+for i in range(start, 0, -1):
+    print(i)
+
+print("Liftoff!")

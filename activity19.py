68fc38b16d1b5f8807aa70c34a539e7aefdb434e
gnmancia <geraldnoelmancia@gmail.com>
2025-10-01 21:06:36 +0800

    Update activity19.py

--- activity19.py
+++ activity19.py
@@ -1,20 +1,10 @@
-print("\t\t *",end=" ")
-for j in range(1, 10, 1):
-    for z in range(10, j, -1):
-        print(" ", end= " ")
-    for x in range(1, j, 1):
-        print("*", end= " ")
-    for g in range(1, j, 1):
-        print("*", end= " ")
-    print()
-    
 
-for j in range(1, 10, 1):
-    for z in range(1, j, 1):
-        print(" ", end= " ")
-    for x in range(10, j, -1):
-        print("*", end= " ")
-    for g in range(10, j, -1):
-        print("*", end= " ")
-    print()
 print("\t\t *",end=" ")
+for s in range(1,11,1):
+    for g in range(10,s,-1):
+        print(" ", end=" ")
+    for h in range(1,s,1):
+        print("*", end=" ")
+    for j in range(1,s,1):
+        print("*", end=" ")
+    print()

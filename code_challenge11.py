ac63613471ab1a4156ea430c88c34b6ec3fdba5d
gnmancia <geraldnoelmancia@gmail.com>
2025-10-01 21:10:52 +0800

    Update code_challenge11.py

--- code_challenge11.py
+++ code_challenge11.py
@@ -1,11 +1,20 @@
-print("\t\t *",end="")
-
-for i in range(1, 11 ,1):
-    for x in range(10, i, -1):
+print("\t\t *",end=" ")
+for s in range(1,10,1):
+    for g in range(10,s,-1):
         print(" ", end=" ")
-    for y in range(1, i, 1):
-        print("*", end = " ")
-    for z in range(1, i, 1):
-        print("*", end = " ")
+    for h in range(1,s,1):
+        print("*", end=" ")
+    for j in range(1,s,1):
+        print("*", end=" ")
     print()
+    
 
+for s in range(1,10,1):
+    for g in range(1,s,1):
+        print(" ", end=" ")
+    for h in range(10,s,-1):
+        print("*", end=" ")
+    for j in range(10,s,-1):
+        print("*", end=" ")
+    print()
+print("\t\t *",end=" ")
